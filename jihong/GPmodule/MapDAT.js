de0918adb16b7a3f393921a29a356b79df0ec72f
BlaCkinkGJ <ss5kijun@gmail.com>
2018-07-31 06:44:50 +0900

    MapDat 수정됨
    기존의 String의 실수값으로의 변경  및 데이터 탐색 실패시의 처리를 추가하였음

--- jihong/GPmodule/MapDAT.js
+++ jihong/GPmodule/MapDAT.js
@@ -26,18 +26,32 @@ class Map{
         };
         const URL = "https://dapi.kakao.com/v2/local/search/address.json"
         client.get(URL, args, (data, res)=>{
-            const most_approximate = data["documents"][0];
-            const road_address = most_approximate["road_address"];
-            const point = {
-                "x" : road_address["x"],
-                "y" : road_address["y"]
+            let point = {
+                "x" : 0.0,
+                "y" : 0.0
             };
+            if(data["documents"].length !== 0) {
+                const most_approximate = data["documents"][0];
+                const road_address = most_approximate["road_address"];
+                point = {
+                    "x": parseFloat(road_address["x"]),
+                    "y": parseFloat(road_address["y"])
+                };
+            }
             return callback(point);
         });
     }
 }
 
-
 module.exports = {
     Map : new Map(config)
 };
+
+/***** USAGE *****
+let map = new Map(config);
+map.findPoint("부산광역시 남구 용호로 42번길 25-15",(data)=>{
+    console.log(data);
+});
+***** USAGE *****/
+
+
